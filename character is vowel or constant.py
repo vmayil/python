b87598f2d88dfc66b7714820c0fc0a8e175a4d92
vmayil <43341540+vmayil@users.noreply.github.com>
2018-09-24 16:47:12 +0530

    Create character is vowel or constant.py

---
+++ character is vowel or constant.py
@@ -0,0 +1,5 @@
+ch=input("Enter any character:")
+if(ch='a' orch='A',ch='e' or ch='E',ch=i or ch='I',ch=o or ch='O',ch='u' or ch='U'):
+    print(ch,"is vowels.")
+else:
+    print(ch,"is not vowels.")
