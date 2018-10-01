e28eb562a79b7d2b75cfd1757a2de663979daaa8
vmayil <43341540+vmayil@users.noreply.github.com>
2018-10-01 16:38:20 +0530

    Create factorial.py

---
+++ factorial.py
@@ -0,0 +1,8 @@
+num=int(input("Enter a number:")
+factorial=1
+if num=0:
+  print("The factorial value is 1")
+else:
+  for i in range (1,num+1`):
+    factorial=factorial*i
+  print("Factorial value is:")
